cd1d62d9a219e78c4fd733627830619005df2e2b
Sav <98275089+Lunaliar@users.noreply.github.com>
2022-12-15 13:43:11 -0500

    Victory!

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,20 +1,20 @@
-import "./app.css";
-import {AiFillHeart, AiOutlineHeart} from "react-icons/ai";
+import './app.css'
+import {AiFillHeart, AiOutlineHeart} from 'react-icons/ai'
 
 type HangmanDrawingProps = {
-	numberOfGuesses: number;
-};
+  numberOfGuesses: number
+}
 
 export function HangmanDrawing({numberOfGuesses}: HangmanDrawingProps) {
-	const lives = 6 - numberOfGuesses;
-	return (
-		<div className="lives">
-			{[...Array(lives)].map((n, i) => (
-				<AiFillHeart key={"full" + i} />
-			))}
-			{[...Array(numberOfGuesses)].map((n, i) => (
-				<AiOutlineHeart key={"empty" + i} />
-			))}
-		</div>
-	);
+  const lives = 6 - numberOfGuesses
+  return (
+    <div className="lives">
+      {[...Array(lives)].map((n, i) => (
+        <AiFillHeart key={'full' + i} />
+      ))}
+      {[...Array(numberOfGuesses)].map((n, i) => (
+        <AiOutlineHeart key={'empty' + i} />
+      ))}
+    </div>
+  )
 }
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,32 +1,32 @@
 type HangmanWordProps = {
-	guessedLetters: string[];
-	wordToGuess: string;
-	reveal?: boolean;
-};
+  guessedLetters: string[]
+  wordToGuess: string
+  reveal?: boolean
+}
 export function HangmanWord({
-	reveal,
-	guessedLetters,
-	wordToGuess,
+  reveal,
+  guessedLetters,
+  wordToGuess,
 }: HangmanWordProps) {
-	return (
-		<div className="word">
-			{wordToGuess.split("").map((letter, index) => {
-				return (
-					<span className="letter-container" key={`${index}|${letter}`}>
-						<span
-							style={{
-								visibility:
-									guessedLetters.includes(letter) || reveal
-										? "visible"
-										: "hidden",
-								color: !guessedLetters.includes(letter) && reveal ? "red" : "",
-							}}
-						>
-							{letter}
-						</span>
-					</span>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="word">
+      {wordToGuess.split('').map((letter, index) => {
+        return (
+          <span className="letter-container" key={`${index}|${letter}`}>
+            <span
+              style={{
+                visibility:
+                  guessedLetters.includes(letter) || reveal
+                    ? 'visible'
+                    : 'hidden',
+                color: !guessedLetters.includes(letter) && reveal ? 'red' : '',
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        )
+      })}
+    </div>
+  )
 }
